7e740d7214ff8140937077e58b46a79d91c70b46
Wellington Barbosa Junior <wellingtonbjr@outlook.com>
2022-07-31 22:35:42 -0300

    Heroku deploy test

--- src/main/java/com/lavajato/spring/api/controllers/AuthController.java
+++ src/main/java/com/lavajato/spring/api/controllers/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://lavajatofrontend.herokuapp.com"}, maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
--- src/main/java/com/lavajato/spring/api/controllers/EnderecoController.java
+++ src/main/java/com/lavajato/spring/api/controllers/EnderecoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://lavajatofrontend.herokuapp.com"}, maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/endereco")
 public class EnderecoController {
